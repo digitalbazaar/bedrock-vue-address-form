6fd164388deaddbf7653ea44e4caf3aadee8040f
Dave Longley <dlongley@digitalbazaar.com>
2019-02-06 19:42:35 -0500

    Add core files.

---
+++ test/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page
+    class="column gutter-md background"
+    padding>
+    <div class="column items-center">
+      <br-address-form />
+    </div>
+  </q-page>
+</template>
+<script>
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import {BrAddressForm} from 'bedrock-vue-address-form';
+
+export default {
+  name: 'Home',
+  components: {BrAddressForm},
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+</style>
