f377b11bac09fcc7728d5caa283f68a3e81cf9ec
Ganesh Annan <gannan@digitalbazaar.com>
2019-02-11 16:18:39 -0500

    Update label generation and use alpha-2 codes for countries.

--- BrAddressForm.vue
+++ BrAddressForm.vue
@@ -4,7 +4,7 @@
       <q-field :error="$v.streetAddress.$error">
         <q-input
           v-model="streetAddress.value"
-          :float-label="streetAddressLabel"
+          :float-label="streetAddress.label"
           class="q-pa-sm q-mt-md"
           @blur="$v.streetAddress.$touch"
           @keyup="$v.streetAddress.$touch" />
@@ -17,7 +17,7 @@
         <q-field :error="$v.addressLocality.$error">
           <q-input
             v-model="addressLocality.value"
-            :float-label="addressLocalityLabel"
+            :float-label="addressLocality.label"
             class="q-pa-sm q-mt-md"
             @blur="$v.addressLocality.$touch"
             @keyup="$v.addressLocality.$touch" />
@@ -27,7 +27,7 @@
         <q-field :error="$v.postalCode.$error">
           <q-input
             v-model="postalCode.value"
-            :float-label="postalCodeLabel"
+            :float-label="postalCode.label"
             class="q-pa-sm q-mt-md"
             @blur="$v.postalCode.$touch"
             @keyup="$v.postalCode.$touch" />
@@ -40,7 +40,7 @@
       <q-field :error="$v.addressLocality.$error">
         <q-input
           v-model="addressLocality.value"
-          :float-label="addressLocalityLabel"
+          :float-label="addressLocality.label"
           class="q-pa-sm q-mt-md"
           @blur="$v.addressLocality.$touch"
           @keyup="$v.addressLocality.$touch" />
@@ -53,7 +53,7 @@
         <q-field :error="$v.addressRegion.$error">
           <q-input
             v-model="addressRegion.value"
-            :float-label="addressRegionLabel"
+            :float-label="addressRegion.label"
             class="q-pa-sm q-mt-md"
             @blur="$v.addressRegion.$touch"
             @keyup="$v.addressRegion.$touch" />
@@ -63,7 +63,7 @@
         <q-field :error="$v.addressCountry.$error">
           <q-select
             v-model="addressCountry.value"
-            :float-label="addressCountryLabel"
+            :float-label="addressCountry.label"
             :options="countries"
             filter
             class="q-pa-sm q-mt-md"
@@ -79,7 +79,7 @@
         <q-field :error="$v.addressRegion.$error">
           <q-select
             v-model="addressRegion.value"
-            :float-label="addressRegionLabel"
+            :float-label="addressRegion.label"
             :options="states"
             filter
             class="q-pa-sm q-mt-md"
@@ -91,7 +91,7 @@
         <q-field :error="$v.postalCode.$error">
           <q-input
             v-model="postalCode.value"
-            :float-label="postalCodeLabel"
+            :float-label="postalCode.label"
             class="q-pa-sm q-mt-md"
             @blur="$v.postalCode.$touch"
             @keyup="$v.postalCode.$touch" />
@@ -110,6 +110,7 @@ import {minLength, required} from 'vuelidate/lib/validators';
 import rawCountryCodes from './countryCodes';
 import states from './states';
 
+
 export default {
   name: 'BrAddressForm',
   props: {
@@ -127,7 +128,6 @@ export default {
   data() {
     return {
       rawCountryCodes,
-      submitted: false,
       states
     };
   },
@@ -200,19 +200,13 @@ export default {
     addressCountryExists() {
       return this.isString(this.addressCountry.value);
     },
-    addressCountryLabel() {
-      return (this.value.addressCountry || {}).label || 'Country';
-    },
     addressLocality() {
       return this.value.addressLocality;
-    },
-    addressLocalityLabel() {
-      return this.value.addressLocality.label || 'City';
     },
     countries() {
       let countries = this.rawCountryCodes.map(val => ({
         label: val.country,
-        value: val.alpha3Code
+        value: val.alpha2Code
       }));
       if(this.restrictCountry.length > 0) {
         countries = countries.filter(
@@ -224,31 +218,50 @@ export default {
     addressRegion() {
       return this.value.addressRegion;
     },
-    addressRegionLabel() {
-      return this.value.addressRegion.label || 'State/Province/Region';
-    },
     postalCode() {
       return this.value.postalCode;
     },
-    postalCodeLabel() {
-      return this.value.postalCode.label || 'ZIP/Postal Code';
-    },
     streetAddress() {
       return this.value.streetAddress;
-    },
-    streetAddressLabel() {
-      return this.value.streetAddress.label || 'Street Address';
     },
     valid() {
       return !this.$v.$invalid;
     }
   },
+  created() {
+    const updatedLabels = _applyDefaultLabels({
+      data: this.value,
+      labels: {
+        addressCountry: 'Country',
+        addressLocality: 'City',
+        addressRegion: 'State/Province/Region',
+        postalCode: 'ZIP/Postal Code',
+        streetAddress: 'Street Address'
+      }
+    });
+    this.$emit('input', updatedLabels);
+  },
   methods: {
     isString(str) {
-      return typeof str === 'string';
+      return isString(str);
     }
   }
 };
+// TODO: Move to bedrock-web-forms
+function isString(str) {
+  return str && typeof str === 'string';
+}
+
+function _applyDefaultLabels({data, labels}) {
+  return Object.keys(data).reduce((acc, key) => {
+    acc[key] = isString(data[key].label) ? data[key] :
+      {
+        ...data[key],
+        label: labels[key]
+      };
+    return acc;
+  }, {});
+}
 </script>
 <style>
 </style>
